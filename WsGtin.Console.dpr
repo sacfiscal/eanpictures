d09a4440e8aa60f4133c0cca2db57a166d8fa67d
Iago César Fernandes Nogueira <iagocesar.sgs@gmail.com>
2023-08-17 08:51:15 -0300

    Criação de projeto Console com report de Memory Leak

--- WsGtin.Console.dpr
+++ WsGtin.Console.dpr
@@ -5,11 +5,22 @@ program WsGtin.Console;
 {$R *.res}
 
 uses
-  System.SysUtils;
+  System.SysUtils,
+  main.control in 'src\main.control.pas',
+  WsGTin.Controllers.Produto in 'src\controllers\WsGTin.Controllers.Produto.pas',
+  WsGTin.Controllers.Registry in 'src\controllers\WsGTin.Controllers.Registry.pas',
+  WsGTin.Controllers.UnidadeMedida in 'src\controllers\WsGTin.Controllers.UnidadeMedida.pas',
+  wsHorse in 'src\wsHorse.pas',
+  WsGTin.Model.Factory in 'src\model\WsGTin.Model.Factory.pas',
+  WsGTin.Model.Interfaces in 'src\model\WsGTin.Model.Interfaces.pas',
+  WsGTin.Model.Produto in 'src\model\WsGTin.Model.Produto.pas',
+  WsGTin.Model.UnidadeMedida in 'src\model\WsGTin.Model.UnidadeMedida.pas',
+  WsGTin.Model.Entity.Produto in 'src\model\entity\WsGTin.Model.Entity.Produto.pas',
+  WsGTin.Model.Entity.UnidadeMedida in 'src\model\entity\WsGTin.Model.Entity.UnidadeMedida.pas';
 
 begin
   try
-    { TODO -oUser -cConsole Main : Insert code here }
+    TMainControl.GetInstance.Power;
   except
     on E: Exception do
       Writeln(E.ClassName, ': ', E.Message);
